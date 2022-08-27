c9e40969e46e7d4c9c178796c0c961df5a3e8e37
thephotogenicbug <navinkumarachari@gmail.com>
2022-08-27 17:08:29 +0530

    Age Calculator Kotline App(Build Simple ui and add Date picker Dialog)

---
+++ app/src/main/java/com/naveen/dobcalc/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.naveen.dobcalc
+
+import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+//            Run DatePicker Function
+            clickDatePicker()
+
+        }
+    }
+//    DatePicker Fun
+    fun clickDatePicker(){
+
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{view, year, month, datOfMonth ->
+
+            Toast.makeText(this,
+                "DatePicker works", Toast.LENGTH_LONG).show()
+           },
+            year,
+            month,
+            day,
+            ).show()
+
+
+    }
+
+}
